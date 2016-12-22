8839399ef3032abcbeebf43d2eaa4d5c8e3b46df
changrong <changrong@changrongdeMacBook-Pro.local>
2016-12-22 22:07:07 +0800

    修改路径

--- update-blog.sh
+++ update-blog.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 #
-who
-pwd
 
-# GIT_PATH=$PWD
-# GIHUB_REPO="git@github.com:croef/blog_source.git"
-# DOCKER_PATH="/srv/docker/blog"
+GIT_PATH="/home/blog"
+GIHUB_REPO="git@github.com:croef/blog_source.git"
+DOCKER_PATH="/srv/docker/blog"
 
-# echo "======== PULL SOURCE ========"
-# cd $GIT_PATH
-# git pull origin
+echo "======== PULL SOURCE ========"
+cd $GIT_PATH
+git pull origin
 
-# echo ""
-# echo "======== CHECK GITHUB REPO ========"
-# git remote
-# g=$(git remote | grep github)
+echo ""
+echo "======== CHECK GITHUB REPO ========"
+git remote
+g=$(git remote | grep github)
 
-# if [ "$g" = "github" ]
-# then
-# 	echo "HAS GITHUB"
-# else
-# 	echo "ADD REMOTE GITHUB"
-# 	git remote add github "$GIHUB_REPO"
-# fi
-# echo "======== PUSH GITHUB ========"
-# git push github
+if [ "$g" = "github" ]
+then
+	echo "HAS GITHUB"
+else
+	echo "ADD REMOTE GITHUB"
+	git remote add github "$GIHUB_REPO"
+fi
+echo "======== PUSH GITHUB ========"
+git push github
 
-# echo ""
-# echo "======== HEXO GENERATE ========"
-# docker run -it --rm -v "$DOCKER_PATH":/root/blog croef/hexo generate
+echo ""
+echo "======== HEXO GENERATE ========"
+docker run -it --rm -v "$DOCKER_PATH":/root/blog croef/hexo generate
