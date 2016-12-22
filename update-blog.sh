3ce1968aed1728beb12bdcab8217f6c4068196c6
changrong <changrong@changrongdeMacBook-Pro.local>
2016-12-22 22:12:45 +0800

    update shell

--- update-blog.sh
+++ update-blog.sh
@@ -5,7 +5,14 @@ GIT_PATH="/home/blog"
 GIHUB_REPO="git@github.com:croef/blog_source.git"
 DOCKER_PATH="/srv/docker/blog"
 
+echo ""
+echo "======== BEGIN ========"
+
+git log --oneline --max-count=10
+
+echo ""
 echo "======== PULL SOURCE ========"
+echo "$GIT_PATH"
 cd $GIT_PATH
 git pull origin
 
