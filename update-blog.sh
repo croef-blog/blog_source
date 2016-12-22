6294c25b0ecffa81f6175b93907b39ed266857a5
changrong <changrong@changrongdeMacBook-Pro.local>
2016-12-22 21:13:01 +0800

    加入update-blog.sh

---
+++ update-blog.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+GIT_PATH=$PWD
+GIHUB_REPO="https://github.com/croef/blog_source.git"
+DOCKER_PATH="/srv/docker/blog"
+
+echo "======== PULL SOURCE ========"
+cd $GIT_PATH
+git pull origin
+
+echo ""
+echo "======== PUSH GITHUB ========"
+git remote
+g=$(git remote | grep github)
+
+if [ "$g" = "github" ]
+then
+	echo "HAS GITHUB"
+else
+	echo "ADD REMOTE GITHUB"
+	git remote add github "$GIHUB_REPO"
+fi
+git push github
+
+echo ""
+echo "======== HEXO GENERATE ========"
+docker run -it --rm -v "$DOCKER_PATH":/root/blog croef/hexo generate
